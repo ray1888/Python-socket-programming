c4b11e72c54466e9f8500ef24220b90941c82038
ray1888 <[alal1995@hotmail.com]>
2017-06-06 10:01:52 +0800

    version 0.0.01

--- client/client.py
+++ client/client.py
@@ -1,29 +1,12 @@
 import socket
 import json
 
-
-port = 8101
-
-content = s.recv(1024)
-print(content)
-print(s)
-state = True
-
-while state:
-    cmd = input('请输入命令')
-    print(cmd)
-    print(type(cmd))
-    s.send(bytes(cmd, encoding="utf-8"))
-    result = s.recv(1024)
-    print(result)
-    if result == b"0":
-        state = False
-        print("you are quit")
-
-
 class Control():
+	def __init__(self):
 
-	def getconfig(self):
+
+
+	def Getconfig(self):
 		Path = os.getcwd()
 		with open(Path+'/clientconfig.json','r') as f:
 			config = f.read()
@@ -32,9 +15,23 @@ class Control():
 			port = config["port"]
 		return (host,port)
 
-	def connect(self):
+	def Connect(self):
 		host,port = getconfig()
 		s = socket.socket()
 		s.connect((host, port))
+		content = s.recv(1024)
+		print(content)
+	
+	def InputCmd(self):
+		state = True
+		while state:
+			cmd = input('请输入命令')
+			s.send(bytes(cmd, encoding="utf-8"))
+    		result = s.recv(1024)
+    		if result == b"0":
+        		state = False
+        		print("you are quit")
+
+    
 		
 
