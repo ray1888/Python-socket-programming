cfca632aca4c495b3c9f2f456fafa0fd8554fee8
ray1888 <[alal1995@hotmail.com]>
2017-06-07 10:42:40 +0800

    version 0.0.03,data tunnel open

--- client/client-connect-test.py
+++ client/client-connect-test.py
@@ -9,8 +9,8 @@ class Control():
         self.s = socket.socket()
         self.pwd = os.getcwd()
         self.Modeselection()
-        self.Connect(self.s, self.mode)
-        self.InputCmd(self.s, self.mode, self.host, self.lport, self.addr)
+        self.Connect(self.mode)
+        self.InputCmd(self.mode, self.host, self.lport, self.addr)
 
     def Modeselection(self):
         mode = input("请输入模式，主动输入ACT，被动输入PASV")
@@ -19,43 +19,46 @@ class Control():
         else:
             self.mode = mode
 
-    def Connect(self, s, mode):
+    def Connect(self, mode):
         print(mode)
         host = "127.0.0.1"
         port = 21
         self.host = host
-        s.connect((host, port))
-        welcome = s.recv(1024)
+        self.s.connect((host, port))
+        welcome = self.s.recv(1024)
         print(welcome)
         modemsg = bytes(mode, encoding="utf-8")
-        s.send(modemsg)
-        a, lport = s.getsockname()
+        self.s.send(modemsg)   #把mode参数从client传到服务器
+        a, lport = self.s.getsockname()
         self.lport = lport
         self.addr = a
+        print(self.lport)
+        print(self.addr)
 
 
-    def CreatPort(self, localport):
+    def CreatePort(self, localport):
         tranport = random.randint(4096,65535)
         if tranport == localport:
-            CreatPort()
+            self.CreatePort(localport)
         else:
             return tranport  #tp为主动模式下被服务器连接的端口
 
-    def InputCmd(self, s, mode, shost, lport=None, laddr=None):
+    def InputCmd(self, mode, shost, lport=None, laddr=None):
         cmd = input("请输入命令")
-        s.send(bytes(cmd, encoding="utf-8"))
+        self.s.send(bytes(cmd, encoding="utf-8"))
         print("cmd sent")
         if mode == "PASV":  #被动模式
-            serverport = s.recv(1024)
+            serverport = self.s.recv(1024)
+            serverport = int(serverport)
             print(serverport)
             ts = socket.socket()
             ts.connect((shost, serverport))
             msg = ts.recv(1024)
             print(msg)
             self.tssock = ts
-
         else:  #主动模式
-            tport = CreatPort(lport)   #tport是传输信道的端口
+            tport = self.CreatePort(lport)   #tport是传输信道的端口
+            self.s.send(bytes(str(tport), encoding="utf-8"))
             tsactive0 = socket.socket()  #tsactive0为等待对方进入的socket
             tsactive0.bind((laddr, tport))
             tsactive0.listen(5)
@@ -63,8 +66,6 @@ class Control():
             self.tssock = tsactive1
             msg_tun = tsactive1.recv(1024)
             print(msg_tun)
-
-
 
 if __name__ == "__main__":
     c = Control()
--- server/server-connect-test.py
+++ server/server-connect-test.py
@@ -20,14 +20,17 @@ class Control():
         self.conn = c
         c.send(b"Welcome to the FTP server")
         mode = c.recv(1024)
-        print(mode)
+        print("mode is {}".format(mode))   #从client接收到的Mode参数
         self.mode = mode
         rmport = addr[1]
         rmaddr = addr[0]
         self.rmport = rmport
         self.rmaddr = rmaddr
-        host, lport = self.s.getsockname()
+        host, lport = self.conn.getpeername()
         self.host = host
+        print(self.rmport)
+        print(self.rmaddr)
+        print(self.host)
 
 
     def CreatPort(self):
@@ -35,29 +38,32 @@ class Control():
         return tranport  #tp为主动模式下被服务器连接的端口
 
     def CmdRec(self, mode, chost, laddr=None):
-        print(self.s.getsockname())
-        cmd = self.s.recv(1024)
+        print(self.conn.getsockname())
+        cmd = self.conn.recv(1024)
         print(cmd)
         if mode == b"PASV":  #被动模式
             tport = self.CreatPort()  # tport是传输信道的端口
-            print("peer={}".format(self.s.getpeername()))
-            s.send(bytes(str(tport), encoding="utf-8"))
+            print("peer={}".format(self.conn.getpeername()))
+            self.conn.send(bytes(str(tport), encoding="utf-8"))
             tsactive0 = socket.socket()  # tsactive0为等待对方进入的socket
             tsactive0.bind((laddr, tport))
             tsactive0.listen(5)
             tsactive1, addrr = tsactive0.accept()
-            tsactive1.send("PASV mode tunnel has been started")
+            tsactive1.send(b"PASV mode tunnel has been started")
             self.tssock = tsactive1
             #msg_tun = tsactive1.recv(1024)
 
-        else:  #主动模式
+        else: #主动模式
             lport = 20
-            serverport = s.recv(1024)
-            ts = socket.socket()
-            ts.bind((laddr, lport))
-            ts.connect((chost, serverport))
-            ts.send("active mode tunnel has been started")
-            self.tssock = ts
+            serverport = self.conn.recv(1024)
+            print(serverport)
+            serverport = int(serverport)
+            print(type(serverport))
+            tunnal_sock = socket.socket()
+            tunnal_sock.bind((laddr, lport))
+            tunnal_sock.connect((chost, serverport))
+            tunnal_sock.send(b"active mode tunnel has been started")
+            self.tssock = tunnal_sock
 
 
 if __name__ == "__main__":
